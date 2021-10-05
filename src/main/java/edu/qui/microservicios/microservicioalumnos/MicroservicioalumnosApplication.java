d82ccfb3a280d98233fcbddf79ec585d600ef5f1
Enrique Sancho <quiquesanchocaballer@gmail.com>
2021-10-05 10:29:16 +0200

    Reconocer Entity
    Añadimos el decorados @EntityScan para que nos reconozca la entidad Alumno que está en común

--- src/main/java/edu/qui/microservicios/microservicioalumnos/MicroservicioalumnosApplication.java
+++ src/main/java/edu/qui/microservicios/microservicioalumnos/MicroservicioalumnosApplication.java
@@ -2,8 +2,10 @@ package edu.qui.microservicios.microservicioalumnos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("edu.qui.microservicios")
 public class MicroservicioalumnosApplication {
 
 	public static void main(String[] args) {
